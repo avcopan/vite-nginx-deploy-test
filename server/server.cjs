5a6b7b73e6baa87fa484c4bc6035ca6a1eb2d2a0
Andreas Copan <avcopan@gmail.com>
2023-10-04 15:35:50 -0400

    Tries enabling CORS on server

--- server/server.cjs
+++ server/server.cjs
@@ -1,8 +1,10 @@
+const cors = require("cors");
 const express = require("express");
 const PORT = 3000 || process.env.PORT;
 
 const app = express();
 
+app.use(cors());
 app.use(express.json());
 
 /* Routes */
